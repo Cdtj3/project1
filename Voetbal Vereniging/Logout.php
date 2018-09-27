eda3d7bd7bf95049f0e3612310c8d2765e1cc4e3
EnricoMeijer61 <enricomeijer61@gmail.com>
2018-09-27 15:07:08 +0200

    added home page

--- Voetbal Vereniging/Logout.php
+++ Voetbal Vereniging/Logout.php
@@ -6,9 +6,3 @@ unset($_SESSION["password"]);
 echo 'You have cleaned session';
 header('Refresh: 2; URL = login.php');
 ?>
-/**
- * Created by PhpStorm.
- * User: achraf
- * Date: 19-9-2018
- * Time: 10:52
- */
--- Voetbal Vereniging/index.php
+++ Voetbal Vereniging/index.php
@@ -1,22 +1,88 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
+
     <head>
-        <!-- Required meta tags -->
+
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
 
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <title>Scrolling Nav - Start Bootstrap Template</title>
 
-        <title>Voetballclub avv</title>
+        <!-- Bootstrap core CSS -->
+        <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Custom styles for this template -->
+        <link href="css/scrolling-nav.css" rel="stylesheet">
+        <link rel="stylesheet" href="css\voetbalstyles.css">
+
     </head>
-    <body>
-        <h1>In development</h1>
 
-        <!-- Optional JavaScript -->
-        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <body id="page-top">
+
+        <!-- Navigation -->
+        <?php require "header.html";?>
+
+        <header>
+        <div class="signup_header">
+            <img src="image/001.jpg">
+        </div>
+        </header>
+
+        <section id="about">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 mx-auto">
+                        <h2>Info?</h2>
+                        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur euismod</p>
+                        <ul>
+                            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur euismod</li>
+                            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur euismod</li>
+                            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur euismod</li>
+                            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur euismod</li>
+                        </ul>
+                        <p>Mauris sagittis, lacus ut porttitor vestibulum, odio risus posuere lorem, a convallis justo neque ut neque. Donec id imperdiet lectus, ut pellentesque mauris. Integer at suscipit lectus. Sed accumsan rutrum arcu, vitae porta dui porta sit amet. Vestibulum vulputate faucibus mauris eget lacinia. Duis non ipsum vel nunc convallis luctus.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="services" class="bg-light">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 mx-auto">
+                        <h2>Services we offer</h2>
+                        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut optio velit inventore, expedita quo laboriosam possimus ea consequatur vitae, doloribus consequuntur ex. Nemo assumenda laborum vel, labore ut velit dignissimos.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="contact">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 mx-auto">
+                        <h2>Contact us</h2>
+                        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero odio fugiat voluptatem dolor, provident officiis, id iusto! Obcaecati incidunt, qui nihil beatae magnam et repudiandae ipsa exercitationem, in, quo totam.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Footer -->
+        <?php require "footer.html";?>
+
+        <!-- Bootstrap core JavaScript -->
+        <script src="vendor/jquery/jquery.min.js"></script>
+        <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+        <!-- Plugin JavaScript -->
+        <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+        <!-- Custom JavaScript for this theme -->
+        <script src="js/scrolling-nav.js"></script>
+
     </body>
+
 </html>
