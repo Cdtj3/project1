445394c4e4ed226d7ec09f8a3cf0eded5637d4be
EnricoMeijer61 <enricomeijer61@gmail.com>
2018-10-10 15:18:50 +0200

    register

--- Voetbal Vereniging/register.php
+++ Voetbal Vereniging/register.php
@@ -16,7 +16,6 @@
 
         <div class="signup_container">
             <form method="post" action="register.php">
-
                 <?php include('errors.php'); ?>
                 <label>Gebruikersnaam</label>
                 <input type="text" name="username" value="<?php echo $username; ?>">
@@ -26,6 +25,20 @@
                 <input type="password" name="password_1">
                 <label>bevestig wachtwoord</label>
                 <input type="password" name="password_2">
+                <label>naam</label>
+                <input type="text" name='name'>
+                <label>gender</label>
+                <input type="radio" name="gender" value="Male"> Man
+                <input type="radio" name="gender" value="Female"> Vrouw
+                <input type="radio" name="gender" value="Other"> Anders
+                <label>membercol</label>
+                <input type="text" name="membercol">
+                <label>type</label>
+                <input type="text" name="type">
+                <label>Rijbewijs</label>
+                <input type="radio" name="year_driving_licence" value="1"> ja
+                <input type="radio" name="year_driving_licence" value="0"> nee
+
                 <button type="submit" class="btn" name="reg_user">registreren</button>
             </form>
         </div>
--- Voetbal Vereniging/server.php
+++ Voetbal Vereniging/server.php
@@ -4,11 +4,16 @@
 	// variable declaration
 	$username = "";
 	$email    = "";
+	$name = "";
+	$gender = "";
+	$membercol = "";
+	$type = "";
+	$year_driving_licence = "";
 	$errors = array(); 
 	$_SESSION['success'] = "";
 
 	// connect to database
-	$db = mysqli_connect('localhost:3307', 'root', 'root', 'registration');
+	$db = mysqli_connect('localhost:3309', 'root', 'root11', 'design_vv_app');
 
 	// REGISTER USER
 	if (isset($_POST['reg_user'])) {
@@ -17,11 +22,20 @@
 		$email = mysqli_real_escape_string($db, $_POST['email']);
 		$password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
 		$password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+		$name = mysqli_real_escape_string($db, $_POST['name']);
+        $gender = mysqli_real_escape_string($db, $_POST['gender']);
+        $membercol = mysqli_real_escape_string($db, $_POST['membercol']);
+        $type = mysqli_real_escape_string($db, $_POST['type']);
+        $year_driving_licence = mysqli_real_escape_string($db, $_POST['year_driving_licence']);
+
+
+
 
 		// form validation: ensure that the form is correctly filled
 		if (empty($username)) { array_push($errors, "Username is required"); }
 		if (empty($email)) { array_push($errors, "Email is required"); }
 		if (empty($password_1)) { array_push($errors, "Password is required"); }
+        if (empty($name)) { array_push($errors, "Name is required"); }
 
 		if ($password_1 != $password_2) {
 			array_push($errors, "The two passwords do not match");
@@ -30,8 +44,8 @@
 		// register user if there are no errors in the form
 		if (count($errors) == 0) {
 			$password = md5($password_1);//encrypt the password before saving in the database
-			$query = "INSERT INTO users (username, email, password) 
-					  VALUES('$username', '$email', '$password')";
+			$query = "INSERT INTO member1 (username, email, password, name, gender, membercol, type, year_driving_licence) 
+					  VALUES('$username', '$email', '$password', '$name', '$gender','$membercol','$type','$year_driving_licence')";
 			mysqli_query($db, $query);
 
 			$_SESSION['username'] = $username;
@@ -57,7 +71,7 @@
 
 		if (count($errors) == 0) {
 			$password = md5($password);
-			$query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+			$query = "SELECT * FROM member1 WHERE username='$username' AND password='$password'";
 			$results = mysqli_query($db, $query);
 
 			if (mysqli_num_rows($results) == 1) {
