fc7233317632130dc2601a2f2d6ddaa12b4b1f77
EnricoMeijer61 <enricomeijer61@gmail.com>
2018-10-10 11:24:05 +0200

    carpool css fix

--- Voetbal Vereniging/carpool.php
+++ Voetbal Vereniging/carpool.php
@@ -3,9 +3,12 @@
 <html>
     <head>
         <link rel="stylesheet" href="css\carpool.css">
-        <img src="https://imgix.ttcdn.co/i/product/original/0/214112-7e4574c9b3af459ab894496f2c3707dd.jpeg?quality=50&width=2000&bg-color=fff&auto=webp&fit=bounds&fm=jpeg" alt="voetbalplaatje" class="voetbalplaatje">
+        <link rel="stylesheet" href="css/head.css">
     </head>
     <body>
+    <div class="signup_header">
+        <img src="https://imgix.ttcdn.co/i/product/original/0/214112-7e4574c9b3af459ab894496f2c3707dd.jpeg?quality=50&width=2000&bg-color=fff&auto=webp&fit=bounds&fm=jpeg" alt="voetbalplaatje" class="voetbalplaatje">
+    </div>
         <h1>Carpool</h1>
     </body>
     <?php require "footer.html";?>
