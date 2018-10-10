560655e282ad5ebb85a0bce7471c8415d7184383
EnricoMeijer61 <enricomeijer61@gmail.com>
2018-10-10 11:12:02 +0200

    merge

--- Voetbal Vereniging/carpool.php
+++ Voetbal Vereniging/carpool.php
@@ -1,14 +1,12 @@
-<?php require "header.html";?>
-<link rel="stylesheet" href="css\head.css">
-<link rel="stylesheet" href="css\carpool.css">
-<body>
-    <header>
-        <div class="signup_header">
-            <img src="image/001.jpg">
-        </div>
-    </header>
-    <div>
-        <p> some text some textsome textsome textsome textsome textsome textsome textsome textsome text</p>
-    </div>
-</body>
-<?php require "footer.html";?>
+<?php include('server.php'); require "header.html";?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="css\carpool.css">
+        <img src="https://imgix.ttcdn.co/i/product/original/0/214112-7e4574c9b3af459ab894496f2c3707dd.jpeg?quality=50&width=2000&bg-color=fff&auto=webp&fit=bounds&fm=jpeg" alt="voetbalplaatje" class="voetbalplaatje">
+    </head>
+    <body>
+        <h1>Carpool</h1>
+    </body>
+    <?php require "footer.html";?>
+</html>
---
+++ Voetbal Vereniging/contact.php
@@ -0,0 +1,24 @@
+<?php require "header.html";?>
+<link rel="stylesheet" href="css\voetbalstyles.css">
+
+<!DOCTYPE html>
+<html lang="en">
+<body>
+<div class="signup_header">
+    <img src="image/001.jpg">
+</div>
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2435.8495672773365!2d5.195198415801997!3d52.37314767978659!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c616d64330f2cf%3A0xdce06b041053202f!2sAS&#39;80!5e0!3m2!1snl!2snl!4v1476449959254" width="100%" height="250" frameborder="0" style="border:0 margin-bottom: 20px;" allowfullscreen></iframe>
+<h2>Adres</h2>
+    <p> Klein Brandt Sportpark 7</p>
+    <p> 1312 JW Almere</p>
+    <p>(036) 5331335</p>
+
+<h2>Postadres</h2>
+   <p> V.V. AS'80</p>
+    <p> Postbus 10034</p>
+    <p> 1301 AA</p>
+    <p>info@as80.nl</p>
+
+</body>
+</html>
