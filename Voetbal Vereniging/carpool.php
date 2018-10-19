5234937df6ae628b19ee91b645ffe497f12bf862
EnricoMeijer61 <enricomeijer61@gmail.com>
2018-10-19 12:06:12 +0200

    added head

--- Voetbal Vereniging/carpool.php
+++ Voetbal Vereniging/carpool.php
@@ -9,39 +9,17 @@ if (!isset($_SESSION['username'])) {
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Voetbal vereniging AS80</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom fonts for this template -->
-    <link href="vendor/font/all.min.css" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
-
-    <!-- Plugin CSS -->
-    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet" type="text/css">
-
-    <!-- Custom styles for this template -->
-    <link href="css/freelancer.min.css" rel="stylesheet">
-
-</head>
-<body id="page-top">
-<!-- Header -->
-<header class="masthead bg-primary text-white text-center">
-    <div class="container">
-        <img class="img-fluid mb-5 d-block mx-auto" src="image/001.jpg" alt="">
-        <h1 class="text-uppercase mb-0">Welke bij ons voetbalteam AS '80</h1>
-        <hr class="star-light">
-        <h2 class="font-weight-light mb-0">Made by team 5</h2>
-    </div>
-</header>
-<?php require "footer.html" ;?>
+    <?php require "head.php" ?>
+    <body id="page-top">
+        <!-- Header -->
+        <header class="masthead bg-primary text-white text-center">
+            <div class="container">
+                <img class="img-fluid mb-5 d-block mx-auto" src="image/001.jpg" alt="">
+                <h1 class="text-uppercase mb-0">Welke bij ons voetbalteam AS '80</h1>
+                <hr class="star-light">
+                <h2 class="font-weight-light mb-0">Made by team 5</h2>
+            </div>
+        </header>
+        <?php require "footer.html" ;?>
+    </body>
+</html>
