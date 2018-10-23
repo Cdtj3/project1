a472034c9cc37f68638e0a5ecb20bbd23dc7cfb7
Enrico Meijer <enricomeijer61@gmail.com>
2018-10-23 15:03:58 +0200

    layout contact

--- Voetbal Vereniging/contact.php
+++ Voetbal Vereniging/contact.php
@@ -1,112 +1,65 @@
-<?php require "require/logincheck.php";?>
-
-<?php require "header.html";?>
-
+<?php //require "require/logincheck.php";?>
+<?php require "header.html"; ?>
 <?php require "head.php" ?>
 <!DOCTYPE html>
-
 <html lang="en">
-
-
 <body id="page-top">
-
 </br>
 </br>
-
 <!-- Contact Section -->
-
 <section id="contact">
-
     <div class="container">
-
         <h2 class="text-center text-uppercase text-secondary mb-0">Contact Me</h2>
-
         <hr class="star-dark mb-5">
-
         <div class="row">
-
             <div class="col-lg-8 mx-auto">
-
-
                 <form name="sentMessage" id="contactForm" novalidate="novalidate">
-
                     <div class="control-group">
-
                         <div class="form-group floating-label-form-group controls mb-0 pb-2">
-
                             <label>Name</label>
-
-                            <input class="form-control" id="name" type="text" placeholder="Name" required="required" data-validation-required-message="Please enter your name.">
-
+                            <input class="form-control" id="name" type="text" placeholder="Name" required="required"
+                                   data-validation-required-message="Please enter your name.">
                             <p class="help-block text-danger"></p>
-
                         </div>
-
                     </div>
-
                     <div class="control-group">
-
                         <div class="form-group floating-label-form-group controls mb-0 pb-2">
-
                             <label>Email Address</label>
-
-                            <input class="form-control" id="email" type="email" placeholder="Email Address" required="required" data-validation-required-message="Please enter your email address.">
-
+                            <input class="form-control" id="email" type="email" placeholder="Email Address"
+                                   required="required"
+                                   data-validation-required-message="Please enter your email address.">
                             <p class="help-block text-danger"></p>
-
                         </div>
-
                     </div>
-
                     <div class="control-group">
-
                         <div class="form-group floating-label-form-group controls mb-0 pb-2">
-
                             <label>Phone Number</label>
-
-                            <input class="form-control" id="phone" type="tel" placeholder="Phone Number" required="required" data-validation-required-message="Please enter your phone number.">
-
+                            <input class="form-control" id="phone" type="tel" placeholder="Phone Number"
+                                   required="required"
+                                   data-validation-required-message="Please enter your phone number.">
                             <p class="help-block text-danger"></p>
-
                         </div>
-
                     </div>
-
                     <div class="control-group">
-
                         <div class="form-group floating-label-form-group controls mb-0 pb-2">
-
                             <label>Message</label>
-
-                            <textarea class="form-control" id="message" rows="5" placeholder="Message" required="required" data-validation-required-message="Please enter a message."></textarea>
-
+                            <textarea class="form-control" id="message" rows="5" placeholder="Message"
+                                      required="required"
+                                      data-validation-required-message="Please enter a message."></textarea>
                             <p class="help-block text-danger"></p>
-
                         </div>
-
                     </div>
-
                     <br>
-
                     <div id="success"></div>
-
                     <div class="form-group">
-
                         <button type="submit" class="btn btn-primary btn-xl" id="sendMessageButton">Send</button>
-
                     </div>
-
                 </form>
-
             </div>
-
         </div>
-
     </div>
-
 </section>
 </body>
-
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2435.8495672773365!2d5.195198415801997!3d52.37314767978659!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c616d64330f2cf%3A0xdce06b041053202f!2sAS&#39;80!5e0!3m2!1snl!2snl!4v1476449959254" width="100%" height="250" frameborder="0" style="border:0 margin-bottom: 20px;" allowfullscreen></iframe>
-
-<?php require "footer.html" ;?>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2435.8495672773365!2d5.195198415801997!3d52.37314767978659!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c616d64330f2cf%3A0xdce06b041053202f!2sAS&#39;80!5e0!3m2!1snl!2snl!4v1476449959254"
+        width="100%" height="250" frameborder="0" style="border:0 margin-bottom: 20px;" allowfullscreen></iframe>
+<?php require "footer.html"; ?>
