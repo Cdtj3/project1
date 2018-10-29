accadc27d149ece61dd5c84935310b3a1145a932
benurk123 <43404853+benurk123@users.noreply.github.com>
2018-10-29 12:54:07 +0100

    Add files via upload

---
+++ server.php
@@ -0,0 +1,28 @@
+<?php 
+	session_start();
+
+	// variable declaration
+	$username = "";
+	$email    = "";
+	$errors = array(); 
+	$_SESSION['success'] = "";
+
+	// connect to database
+//        $servername = "127.0.0.1";
+//        $user = "2018_P1_05";
+//        $pass = "TYfoquCo!H7otA";
+//        $database = "2018_P1_05_design_vv";
+    $servername = "localhost:3307";
+    $user = "root";
+    $pass = "root";
+    $database = "2018_p1_05_design_vv";
+
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$database", $user, $pass);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connected successfully";
+    } catch(PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+    }
+?>
